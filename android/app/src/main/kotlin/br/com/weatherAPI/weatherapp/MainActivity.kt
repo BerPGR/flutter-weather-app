824a599eee6b3f38304b4c3f5e907f2ec1c1cdf1
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2023-05-12 10:02:50 -0300

    Project created

---
+++ android/app/src/main/kotlin/br/com/weatherAPI/weatherapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.weatherAPI.weatherapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFf9f9f9),
+      appBar: AppBar(
+        backgroundColor: Color(0xFFf9f9f9),
+        elevation: 0.0,
+        title: const Text(
+          "Weather App",
+          style: TextStyle(color: Colors.black),
+        ),
+        centerTitle: true,
+        leading: IconButton(
+            icon: Icon(
+              Icons.menu,
+              color: Colors.black,
+            ),
+            onPressed: () {}),
+      ),
+    );
+  }
+}
